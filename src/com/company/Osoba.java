6bd37dc3925f38631426db344de51a3c2b57f049
Kacperna <113970523+Kacperna@users.noreply.github.com>
2025-11-06 21:02:08 +0100

    Initial commit

---
+++ src/com/company/Osoba.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public abstract class Osoba {
+    //abstract - nie można utworzyć obiektu z tej klasy
+    private String imie;
+    private String nazwisko;
+
+    public Osoba(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+
+    public Osoba() {
+        imie="Edward";
+        nazwisko = "Niziurski";
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    @Override
+    public String toString() {
+        return "Osoba{" +
+                "imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                '}';
+    }
+}
